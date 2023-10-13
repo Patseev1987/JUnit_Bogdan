f651dc257b6ab98b293fed393f343c0fc259d70a
Patseev1987 <Patseev1987@gmal.com>
2023-10-13 16:26:19 +0300

    the first lesson

---
+++ src/seminars/first/homework/Main.java
@@ -0,0 +1,9 @@
+package seminars.first.homework;
+
+import seminars.first.Calculator.Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Price is %.2f",Calculator.calculatingDiscount(-10,100));
+    }
+}
